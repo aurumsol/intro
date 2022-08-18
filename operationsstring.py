e6454d2b4e8036a813044d5c900a08b283a97c79
aurumsol <2110030374@klh.edu.in>
2022-08-18 11:09:25 +0530

    String function

---
+++ operationsstring.py
@@ -0,0 +1,37 @@
+#2110030374
+print("Description")
+print("The expandtabs() method returns a copy of the string in which the tab characters")
+print("Syntax=str.expandtabs")
+print("Examples using string function expand tab")
+#example1
+print("example 1")
+str="I am \tur classmate"
+print ("Original string: " + str)
+print ("Defualt exapanded tab: " +  str.expandtabs())
+print ("Double exapanded tab: " +  str.expandtabs(5))
+#example 2
+print("example 2")
+str="I work \ton datasciences"
+print ("Original string: " + str)
+print ("Defualt exapanded tab: " +  str.expandtabs(4))
+print ("Double exapanded tab: " +  str.expandtabs(7))
+#example 3
+print("example 3")
+str="I am \ta data Scientist"
+print ("Original string: " + str)
+print ("Defualt exapanded tab: " +  str.expandtabs(8))
+print ("Double exapanded tab: " +  str.expandtabs(7))
+#example 4
+print("example 4")
+str="I am \t fine"
+print ("Original string: " + str)
+print ("Defualt exapanded tab: " +  str.expandtabs(4))
+print ("Double exapanded tab: " +  str.expandtabs(5))
+#example 5
+print("example 5")
+str="Hello \tworld"
+print ("Original string: " + str)
+print ("Defualt exapanded tab: " +  str.expandtabs(7))
+print ("Double exapanded tab: " +  str.expandtabs(9))
+
+
